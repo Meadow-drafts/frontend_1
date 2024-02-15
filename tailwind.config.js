d7c347df7e63a23586e57d581d2afa20a697cbac
Shirley <shirleylaurette1@gmail.com>
2024-02-15 15:07:03 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,28 +6,27 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        
-          marineBlue: 'hsl(213, 96%, 18%)',
-          purplishBlue: 'hsl(243, 100%, 62%)',
-          pastelBlue: 'hsl(228, 100%, 84%)',
-          lightBlue: 'hsl(206, 94%, 87%)',
-          strawberryRed: 'hsl(354, 84%, 57%)',
-       
-          coolGray: 'hsl(231, 11%, 63%)',
-          lightGray: 'hsl(229, 24%, 87%)',
-          magnolia: 'hsl(217, 100%, 97%)',
-          alabaster: 'hsl(231, 100%, 99%)',
-          white: 'hsl(0, 0%, 100%)',
-        
+        primary: {
+          marineBlue: "hsl(213, 96%, 18%)",
+          purplishBlue: "hsl(243, 100%, 62%)",
+          pastelBlue: "hsl(228, 100%, 84%)",
+          lightBlue: "hsl(206, 94%, 87%)",
+          strawberryRed: "hsl(354, 84%, 57%)",
+        },
+        neutral: {
+          coolGray: "hsl(231, 11%, 63%)",
+          lightGray: "hsl(229, 24%, 87%)",
+          magnolia: "hsl(217, 100%, 97%)",
+          alabaster: "hsl(231, 100%, 99%)",
+        },
       },
-      fontSize: {
-        'body-copy': '16px',
-        // Add more typography styles as needed
+      screens: {
+        sm: "768px",
       },
-      fontFamily:{
-        'body': [   "Inter", 'sans-serif'
-      ]
-      }
+      backgroundImage: theme => ({
+        "desktop": "url('./assets/bg-sidebar-desktop.svg')",
+        "mobile": "url('./assets/bg-sidebar-mobile.svg')",
+      }),
     },
     
   },
